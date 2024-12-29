78f980a84737032c8cf544627638af37b7390c8a
Ali Ibragim <ibragimali768@gmail.com>
2024-12-29 20:59:37 +0300

    Project6Prt1

--- src/historyManager/HistoryManager.java
+++ src/historyManager/HistoryManager.java
@@ -1,7 +1,9 @@
 package historyManager;
 import java.util.List;
 import Tasks.*;
+
 public interface HistoryManager {
     void add(Task task);
+    void remove(int id);
     List<Task> getHistory();
 }
